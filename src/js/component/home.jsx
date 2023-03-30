af52f85bff622c2496ecb13441fb83b002561881
GorgGoddess <jackson.anna1220@gmail.com>
2023-03-30 13:27:13 +0000

    changes

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,22 +5,22 @@ const Home = (props) => {
   console.log(props.sec);
   return (
     <div className="container mx-5 my-4 bg-light d-flex justify-content-evenly fs-5">
-      <h1 className="counter text-black border rounded border-blue">
+      <h1 className="counter text-black border rounded border-black">
         {Math.floor(props.sec / 100000) % 10}
       </h1>
-      <h1 className="counter text-black border rounded  border-blue">
+      <h1 className="counter text-black border rounded  border-black">
         {Math.floor(props.sec / 10000) % 10} 
       </h1>
-      <h1 className="counter text-black border rounded  border-blue">
+      <h1 className="counter text-black border rounded  border-black">
         {Math.floor(props.sec / 1000) % 10} 
       </h1>
-      <h1 className="counter text-black border rounded  border-blue">
+      <h1 className="counter text-black border rounded  border-black">
         {Math.floor(props.sec / 100) % 10} 
       </h1>
-      <h1 className="counter text-black border  rounded border-blue">
+      <h1 className="counter text-black border  rounded border-black">
         {Math.floor(props.sec / 10) % 10} 
       </h1>
-      <h1 className="counter text-black border rounded  border-blue">
+      <h1 className="counter text-black border rounded  border-black">
         {Math.floor(props.sec / 1) % 10} 
       </h1>
       
